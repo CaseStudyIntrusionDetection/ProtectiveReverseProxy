65723b753609bbe20ac236e1cbcc0c86c7c62c87
KIMB-technologies <info@kimb-technologies.eu>
2021-02-24 09:17:38 +0100

    Choose Block crawling

--- proxy/request_check.py
+++ proxy/request_check.py
@@ -10,19 +10,25 @@ class RequestChecker():
 	MODELS_DIR = "/protection/model/"
 
 	def __init__(self):
-		if not os.path.isdir(RequestChecker.MODELS_DIR) or not os.path.isfile(RequestChecker.MODELS_DIR + "/index.json"):
+		if not os.path.isdir(RequestChecker.MODELS_DIR) or not os.path.isfile(RequestChecker.MODELS_DIR + "index.json"):
 			Logging.log("Missing Model!", Logging.LEVEL_ERROR)
 			exit()
 
-		self.models = json.load(open(RequestChecker.MODELS_DIR + "/index.json", 'r'))
-		if 'lda' not in self.models or 'nn' not in self.models or 'name' not in self.models:
+		self.models = json.load(open(RequestChecker.MODELS_DIR + "index.json", 'r'))
+		if 'lda' not in self.models or 'name' not in self.models or 'nn-crawl' not in self.models or 'nn-attack' not in self.models  or 'nn-types' not in self.models:
 			Logging.log("Invalid Model Index!", Logging.LEVEL_ERROR)
 			exit()
 
 		Logging.log("Found Model " + self.models['name'], Logging.LEVEL_INFO)
 
 		self.lda = LDAPredictor(self.models['lda'], RequestChecker.MODELS_DIR)
-		self.nn = NNPredictor(self.models['nn'], RequestChecker.MODELS_DIR)
+		if os.environ.get("BLOCK_CRAWLING") == "true":
+			self.nn = NNPredictor(self.models['nn-crawl'], RequestChecker.MODELS_DIR)
+		else:
+			self.nn = NNPredictor(self.models['nn-attack'], RequestChecker.MODELS_DIR)
+
+		if "BLOCK_TYPES" in os.environ or "ALLOW_TYPES" in os.environ:
+			self.nn_types = NNPredictor(self.models['nn-types'], RequestChecker.MODELS_DIR)
 
 	def is_save(self, request_data):
 
