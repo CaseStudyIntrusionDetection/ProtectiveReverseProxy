75ef881922831d05bcaa2b1e223133b1a9873804
KIMB-technologies <info@kimb-technologies.eu>
2021-03-02 22:32:03 +0100

    Filter on attack types

--- proxy/request_check.py
+++ proxy/request_check.py
@@ -1,6 +1,7 @@
 import os, json
 
 from log import Logging
+from types import TypeHandler
 
 from src.models.predict_model_lda import LDAPredictor
 from src.models.predict_model_nn import NNPredictor
@@ -10,10 +11,11 @@ class RequestChecker():
 	MODELS_DIR = "/protection/model/"
 
 	def __init__(self):
+		# check models directory and index
 		if not os.path.isdir(RequestChecker.MODELS_DIR) or not os.path.isfile(RequestChecker.MODELS_DIR + "index.json"):
 			Logging.log("Missing Model!", Logging.LEVEL_ERROR)
 			exit()
-
+		
 		self.models = json.load(open(RequestChecker.MODELS_DIR + "index.json", 'r'))
 		if 'lda' not in self.models or 'name' not in self.models or 'nn-crawl' not in self.models or 'nn-attack' not in self.models  or 'nn-types' not in self.models:
 			Logging.log("Invalid Model Index!", Logging.LEVEL_ERROR)
@@ -21,23 +23,54 @@ class RequestChecker():
 
 		Logging.log("Found Model " + self.models['name'], Logging.LEVEL_INFO)
 
+		# load default 2 class models
 		self.lda = LDAPredictor(self.models['lda'], RequestChecker.MODELS_DIR)
 		if os.environ.get("BLOCK_CRAWLING") == "true":
 			self.nn = NNPredictor(self.models['nn-crawl'], RequestChecker.MODELS_DIR)
 		else:
 			self.nn = NNPredictor(self.models['nn-attack'], RequestChecker.MODELS_DIR)
 
-		if "BLOCK_TYPES" in os.environ or "ALLOW_TYPES" in os.environ:
+		# load models to get type
+		self.type_handling = TypeHandler()
+		if self.type_handling.is_active():
 			self.nn_types = NNPredictor(self.models['nn-types'], RequestChecker.MODELS_DIR)
+
+		# connector to use for models
+		if "APPROACH_CONNECTOR" in os.environ:
+			self.connector = "and" if os.environ.get("APPROACH_CONNECTOR") == "and" else "or" 
+
+	def model_connector(self, lda_bool, nn_bool):
+		if self.connector == "and":
+			return lda_bool and nn_bool
+		else:
+			return lda_bool or nn_bool
 
 	def is_save(self, request_data):
 
-		lda_is_attack, lda_predictions = self.lda.predict(request_data)
-		print(lda_is_attack, lda_predictions)
+		# two class prediction
+		lda_is_attack, lda_types = self.lda.predict(request_data)
+		nn_is_attack, _ = self.nn.predict(request_data)
 
-		nn_is_attack, nn_predictions = self.nn.predict(request_data)
-		print(nn_is_attack, nn_predictions)
+		if self.type_handling.is_active():
+			_, nn_types = self.nn_types.predict(request_data)
 
-		Logging.log( json.dumps( { 'nn' : (nn_is_attack, nn_predictions), 'lda' : (lda_is_attack, lda_predictions)}) , Logging.LEVEL_ERROR )
+			for lda_type,nn_type in zip(lda_types,nn_types):
+				
+				# check blocked types
+				no_block = self.model_connector(
+					lda_type[0] not in self.type_handling.block_types,
+					nn_type[0] not in self.type_handling.block_types
+				)
+				if not no_block:
+					return False
+
+				# check allowed types
+				direct_allow = self.model_connector(
+					lda_type[0] in self.type_handling.allow_types,
+					nn_type[0] in self.type_handling.allow_types
+				)
+				if direct_allow:
+					return True
 		
-		return not lda_is_attack or not nn_is_attack
+		# default handling
+		return self.model_connector( not lda_is_attack, not nn_is_attack)
---
+++ proxy/types.py
@@ -0,0 +1,88 @@
+import os
+
+from log import Logging
+
+class TypeHandler():
+
+	all_types = {
+		"sql" : [
+			"SQL Injection", "40018",
+			"SQL Injection - Hypersonic SQL", "40020",
+			"SQL Injection - MsSQL", "40027",
+			"SQL Injection - MySQL", "40019",
+			"SQL Injection - Oracle", "40021",
+			"SQL Injection - PostgreSQL", "40022",
+			"SQL Injection - SQLite", "40024",
+			"sqli"
+		],
+		"xss" : [
+			"Cross Site Scripting (Persistent) - Prime", "40016",
+			"Cross Site Scripting (Reflected)", "40012",
+			"Anti-CSRF Tokens Check", "20012",
+			"xss"
+		],
+		"tampering" : {
+			"Parameter Tampering", "40008",
+			"Cookie Slack Detector", "90027"
+			"CRLF Injection", "40003",
+			"User Agent Fuzzer", "10104",
+			"crlf"
+		}
+		"execution" : [
+			"Remote Code Execution - Shell Shock", "10048",
+			"Server Side Code Injection", "90019",
+			"Server Side Include", "40009",
+			"XPath Injection", "90021",
+			"XSLT Injection", "90017",
+			"Expression Language Injection", "90025",
+			"Remote File Inclusion", "7",
+			"Remote OS Command Injection", "90020",
+			"Format String Error", "30002",
+			"cmd_exec",
+			"rfi",
+			"php_code_injection"
+		],
+		"disclosure" : [
+			"Source Code Disclosure - File Inclusion", "43",
+			"Path Traversal", "6",
+			"lfi"
+		],
+		"overflows" : [
+			"Buffer Overflow", "30001",
+			"Integer Overflow Error", "30003",
+		],
+		"encryption" : [
+			"Generic Padding Oracle", "90024"
+		],
+		"cve" : [
+			"Apache Range Header DoS (CVE-2011-3192)", "10053",
+			"error"
+		],
+		"redirect" : [
+			"External Redirect", "20019",
+			"Httpoxy - Proxy Header Misuse", "10107"
+		]
+	}
+
+	def __init__(self):
+		self.block_types = self.load_types("BLOCK_TYPES")
+		self.allow_types = self.load_types("ALLOW_TYPES")
+
+		if len(list(set(self.block_types) & set(self.allow_types))) > 0:
+			Logging.log( "Some types of requests are listed as allowed and also as blocked - choose one!" , Logging.LEVEL_ERROR )
+			exit()
+
+	def load_types(self, env_key):
+		types = []
+		if env_key in os.environ:
+			tl = os.environ.get(env_key)
+			if len(tl) > 0:
+				for t in tl.split(','):
+					if t in TypeHandler.all_types:
+						types.extend(TypeHandler.all_types[t])
+		return types
+
+	def is_active(self):
+		return len(self.block_types) > 0 or len(self.allow_types) > 0 
+
+		
