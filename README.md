898f0444a8bd81077bebb0fb3e0f69849826741c
KIMB-technologies <info@kimb-technologies.eu>
2021-03-04 14:10:16 +0100

    Mail sending works

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ProtectiveReverseProxy
 The protective reverse proxy to place in front of a web service to protect it.
+
+
+## Configuration
+
+> After configuring the STMP E-Mail notifications one may send a testmail by 
+> running `docker exec --user www-data protection_proxy python /proxy/mail.py`.
+> One may run `python /proxy/mail.py --debug` to get debug output.
+
+> Logs can be found at `/tmp/prp.log` and printed by
+> `docker exec --user www-data protection_proxy tail -n 20 /tmp/prp.log`
--- proxy/mail.py
+++ proxy/mail.py
@@ -1,30 +1,37 @@
-import smtplib
+import smtplib, os, sys
 
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
 
 from log import Logging
 
 class Mailer():
 
 	def __init__(self):
+		for k in ['MAIL_HOST', 'MAIL_PORT', 'MAIL_USERNAME', 'MAIL_PASSWORD', 'MAIL_FROM', 'MAIL_TO']:
+			if k not in os.environ:
+				Logging.log("Missing value "+ k +" in environment for mail configuration!", Logging.LEVEL_ERROR)
 
-		self.server = "system"
-      	self.port = int("587")
-		self.user = "aaa@system"
-		self.password = "secret"
-		self.sender = "me@system"
-		self.to = "admin@system"
+		self.server = os.environ.get("MAIL_HOST") 
+		self.port = int(os.environ.get("MAIL_PORT"))
+		self.user = os.environ.get("MAIL_USERNAME") 
+		self.password = os.environ.get("MAIL_PASSWORD")
+		self.sender = os.environ.get("MAIL_FROM")
+		self.to = os.environ.get("MAIL_TO")
 
-	def send(self, text, subject=""):
+	def send(self, text, subject="", debug=False):
 		try:
 			msg = MIMEMultipart()
 			msg['From'] = self.sender
 			msg['To'] = self.to
+			msg['Date'] = formatdate()
 			msg['Subject'] = "[PRP] " + (subject if len(subject) > 0 else "Notification")
 			msg.attach(MIMEText(text))
 
 			client = smtplib.SMTP(host=self.server, port=self.port)
+			if debug:
+				client.set_debuglevel(1)
 			client.ehlo()
 			client.starttls()
 			client.ehlo()
@@ -36,4 +43,12 @@ class Mailer():
 				client.quit()
 
 		except:
-			Logging.log("Error sending mail!", Logging.LEVEL_WARN)
+			Logging.log("Error sending mail!", Logging.LEVEL_ERROR)
+
+if __name__ == "__main__":
+	mailer = Mailer()
+	mailer.send(
+		"Hello user,\nthis is a testmail to verify that SMTP is configured correctly.\n\nProtective Reverse Proxy",
+		"Testmail",
+		debug=len(sys.argv) > 1 and sys.argv[1] == "--debug"
+	)
